03bb6e1c31022a16c5d900a1e1bf0adfd2805853
brunoAyllon <thetrue.kaiserafini@gmail.com>
2015-02-14 13:18:35 -0200

    Reset Camera Position
    So quick to implement it wasn't even funny.

---
+++ Trikroma/Assets/Standard Assets/Scripts/Camera Scripts/CameraResetPosition.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+private var initialPosition:Vector3;
+
+public function ResetPosition()
+{
+	transform.position = initialPosition;
+}
+
+function Start () 
+{
+	initialPosition = transform.position;
+}
+
+function Update () 
+{
+
+}
