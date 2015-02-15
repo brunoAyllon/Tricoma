6f1dc441425ce2355745ed3cb88a4921ff3ded14
banerjeesw <banerjeesw@gmail.com>
2015-02-15 19:11:21 +0700

    Juggernaut Commit Stage 03
    All the Hint text files for our 24 Puzzles. Still need to actually write
    the Hints in the Text box.

--- Trikroma/Assets/Standard Assets/Scripts/Camera Scripts/CameraResetPosition.js
+++ Trikroma/Assets/Standard Assets/Scripts/Camera Scripts/CameraResetPosition.js
@@ -1,15 +1,18 @@
 ﻿#pragma strict
 
 private var initialPosition:Vector3;
+private var initialOrthoSize:float;
 
 public function ResetPosition()
 {
 	transform.position = initialPosition;
+	camera.orthographicSize = initialOrthoSize;
 }
 
 function Start () 
 {
 	initialPosition = transform.position;
+	initialOrthoSize = camera.orthographicSize;
 }
 
 function Update () 
